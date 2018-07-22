b3d30e652225fa17cfd40e9dc0ba729dbb641ca4
lilach vald <lilachvald@gmail.com>
2018-07-22 22:13:07 +0300

    add TestBattleship.py and TestBoard.py

---
+++ TestBattleship.py
@@ -0,0 +1,47 @@
+import unittest
+from battleship import Battleship
+
+
+class TestBattleship(unittest.TestCase):
+
+    def setUp(self):
+        self.battleship = Battleship(['00', '01', '02', '03'])
+
+    def test_contains_Z_before(self):
+        self.assertEqual(self.battleship.contains('00'), 'Z')
+
+    def test_contains_X_after(self):
+        self.battleship.hit('00')
+        self.assertEqual(self.battleship.contains('00'), 'X')
+
+    def test_contains_O_before(self):
+        self.assertEqual(self.battleship.contains('04'), 'O')
+
+    def test_contains_O_after(self):
+        self.battleship.hit('04')
+        self.assertEqual(self.battleship.contains('04'), 'O')
+
+    def test_hit_if_miss(self):
+        self.assertEqual(self.battleship.hit('07'), False)
+
+    def test_hit_if_hit(self):
+        self.assertEqual(self.battleship.hit('01'), True)
+
+    def test_hit_twice(self):
+        self.battleship.hit('01')
+        self.assertEqual(self.battleship.hit('01'), False)
+
+    def test_hit_if_not_sunk(self):
+        self.battleship.hit('01')
+        self.assertEqual(self.battleship.is_sunk, False)
+
+    def test_hit_if_sunk(self):
+        self.battleship.hit('01')
+        self.battleship.hit('03')
+        self.battleship.hit('02')
+        self.battleship.hit('00')
+        self.assertEqual(self.battleship.is_sunk, True)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ TestBoard.py
@@ -0,0 +1,61 @@
+import unittest
+from battleship_boards import Board
+
+
+class TestBoard(unittest.TestCase):
+
+    def setUp(self):
+        self.board = Board()
+        self.board.place_battleship(5, 'A', 1, 'vertical')
+        self.board.place_battleship(4, 'A', 3, 'vertical')
+        self.board.place_battleship(3, 'A', 5, 'vertical')
+
+    def test_find_battleship_if_not_there(self):
+        self.assertEqual(self.board.find_battleship(0, 3), 'O')
+
+    def test_find_battleship_if_there(self):
+        self.assertEqual(self.board.find_battleship(0, 0), 'Z')
+
+    def test_find_battleship_if_there_and_already_hit(self):
+        self.board.guess('A', 1)
+        self.assertEqual(self.board.find_battleship(0, 0), 'X')
+
+    def test_place_battleship_vertical_can_be_placed(self):
+        self.assertEqual(self.board.place_battleship(2, 'A', 8, 'vertical'), True)
+
+    def test_place_battleship_vertical_cannot_be_placed(self):
+        self.assertEqual(self.board.place_battleship(2, 'A', 2, 'vertical'), False)
+
+    def test_place_battleship_horizontal_can_be_placed(self):
+        self.assertEqual(self.board.place_battleship(2, 'H', 6, 'horizontal'), True)
+
+    def test_place_battleship_horizontal_cannot_be_placed(self):
+        self.assertEqual(self.board.place_battleship(2, 'F', 2, 'horizontal'), False)
+
+    def test_place_battleship_horizontal_on_edge(self):
+        self.assertEqual(self.board.place_battleship(2, 'I', 10, 'horizontal'), False)
+
+    def test_guess_already_guess_and_miss(self):
+        self.board.guess('A', 2)
+        self.assertEqual(self.board.guess('A', 2), 1)
+
+    def test_guess_already_guess_and_hit(self):
+        self.board.guess('A', 1)
+        self.assertEqual(self.board.guess('A', 1), 1)
+
+    def test_guess_and_miss(self):
+        self.assertEqual(self.board.guess('A', 2), 2)
+
+    def test_guess_and_hit(self):
+        self.assertEqual(self.board.guess('A', 1), 3)
+
+    def test_guess_and_sunk(self):
+        self.board.guess('A', 1)
+        self.board.guess('B', 1)
+        self.board.guess('C', 1)
+        self.board.guess('D', 1)
+        self.assertEqual(self.board.guess('E', 1), 4)
+
+
+if __name__ == '__main__':
+    unittest.main()
